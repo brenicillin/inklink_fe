0bd373a94b9819fcdb6a7e390308cfaf74f0d9fa
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-02 11:04:32 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   root to: "home#index"
+  get '/login', to: 'sessions#new'
+  resources :sessions, only: [:create, :destroy]
 
   resources :artists, only: [:new, :create]
   resources :users, only: [:new, :create]
