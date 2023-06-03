6187d94c6fbc9c7bc2bd6c740cfefb6e44f795c6
Angel Byun <abyun22@turing.edu>
2023-06-03 09:13:59 -0600

    Create a logout method in sessions; logout button will populate once a user is successfully logged in; once user clicks the logout, user will be taken back to the root_path and login button should be present

--- spec/features/sessions/delete_spec.rb
+++ spec/features/sessions/delete_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "Logout Page" do
   describe "Logout Page" do
     it "creates a button to logout once a user has logged in successfully" do
-      user = Users.create!(name: "Angel Byun", username: "angelbyun", email: "angelb12345@gmail.com", password: "12345", verify_password: "12345", city: "Denver")
+      user = User.create!(name: "Angel Byun", username: "angelbyun", email: "angelb12345@gmail.com", password: "12345", password_confirmation: "12345", city: "Denver")
       
       visit root_path
 
@@ -15,7 +15,11 @@ RSpec.describe "Logout Page" do
       click_button "Submit"
 
       expect(current_path).to eq(root_path)
-      expect(page).to have_content("Welcome, #{user.name}!")
+      expect(page).to have_button("Logout")
+
+      click_button "Logout"
+      
+      expect(current_path).to eq(root_path)
     end
   end
 end
