bd0224b4619a9d7f4602133fb61438efa2020ae7
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-04 18:09:01 -0400

    WIP creating artist through API call

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,5 +3,20 @@ class ArtistsController < ApplicationController
   end
 
   def create
+    conn = Faraday.new(url: "https://inklink-be.herokuapp.com/api/v0/")
+    artist = {
+        name: params[:name],
+        email: params[:email],
+        password: params[:password],
+        confirm_password: params[:confirm_password],
+        pricing: params[:pricing],
+        styles: params[:styles],
+    }
+    response = conn.post('artists', 'artist' => artist)
+    redirect_to root_path
+    end
+
+  def show
+    
   end
 end
