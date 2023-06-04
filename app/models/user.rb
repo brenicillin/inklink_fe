b1a3f0c38e993c23a40c66b1e2d9354d6b9a8bea
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-04 08:26:31 -0400

    add from_omniauth method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
 
   has_secure_password
+
+  def self.from_omniauth(response)
+    User.find_or_create_by(uid: response[:uid], provider: response[:provider]) do |user|
+      user.name = response[:info][:name]
+      user.email = response[:info][:email]
+      user.password = SecureRandom.hex(15)
+    end
+  end
 end
