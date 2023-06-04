3832e82720ebd89e39f417d41e62c421210f73f0
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-04 10:58:48 -0400

    standardize forms and remove username attribute

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,11 +3,26 @@ require 'rails_helper'
 RSpec.describe "User Dashboard Page" do
   describe "User Dashboard" do
     it "has an option for user to search for artist by style/location" do
-      user = User.create!(name: "Angel Byun", username: "angelbyun", email: "angelb12345@gmail.com", password: "12345", password_confirmation: "12345", city: "Denver")
+      user = User.create!(name: "Angel Byun", email: "angelb12345@gmail.com", password: "12345", password_confirmation: "12345", city: "Denver")
 
       visit user_path(user)
 
       expect(page).to have_content("Search")
+    end
+    
+    it 'I see a section to discover tattoos' do
+      user = User.create!(name: "Bob", email: "bob@bob.com", password: "password", password_confirmation: "password", city: "Denver")
+
+      visit '/login'
+
+      fill_in :email, with: user.email
+      fill_in :password, with: user.password
+      
+      click_button 'Submit'
+
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_button('Search')
+      expect(page).to have_selector(:link_or_button, 'Search')
     end
   end
 end
