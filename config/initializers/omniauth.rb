027a4c242577d14957f13b11d59d5a8e42d09dae
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-04 16:21:27 -0400

    update omniauth again

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :google_oauth2, 510841049656-dan93jl9d2a24itrpvnri1b7i4d736k4.apps.googleusercontent.com, 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
+    provider :google_oauth2, "510841049656-dan93jl9d2a24itrpvnri1b7i4d736k4.apps.googleusercontent.com", "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"
   end
 OmniAuth.config.allowed_request_methods = [:get]
