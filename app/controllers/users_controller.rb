0751226884fc8224337918af80ef6a881412c939
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-06-04 18:10:00 -0400

    WIP creating artist through API call

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
   end
   
   def new
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_to user_path(user)
+    else
+      flash[:alert] = "All fields must be filled in."
+      redirect_to new_user_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation, :city)
   end
 end
