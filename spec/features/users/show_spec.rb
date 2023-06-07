71358a564228b1bf027ac54266be9d5103d8c283
Angel Byun <abyun22@turing.edu>
2023-06-06 18:13:21 -0600

    remove "city" from users

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "User Dashboard Page" do
   describe "User Dashboard" do
     it "has an option for user to search for artist by style/location" do
-      user = User.create!(name: "Angel Byun", email: "angelb12345@gmail.com", password: "12345", password_confirmation: "12345", city: "Denver")
+      user = User.create!(name: "Angel Byun", email: "angelb12345@gmail.com", password: "12345", password_confirmation: "12345")
 
       visit user_path(user)
 
@@ -11,7 +11,7 @@ RSpec.describe "User Dashboard Page" do
     end
     
     it 'I see a section to discover tattoos' do
-      user = User.create!(name: "Bob", email: "bob@bob.com", password: "password", password_confirmation: "password", city: "Denver")
+      user = User.create!(name: "Bob", email: "bob@bob.com", password: "password", password_confirmation: "password")
 
       visit '/login'
 
